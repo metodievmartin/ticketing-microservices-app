b39dafd9faed7468e4a4034860c02c4539cf64e3
Martin Metodiev <martin.metodiew@gmail.com>
2021-11-11 16:21:15 +0200

    feat(auth-srv): set up and scaffold routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,43 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'express';
+import mongoose from 'mongoose';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
+// - GLOBAL MIDDLEWARE -
 app.use(json());
 
-app.get('/api/users/currentuser', ((req, res) => {
-  res.send('Hi there');
-}));
+// - ROUTE HANDLERS -
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.listen(3000, () => {
-  console.log('Auth service listening on port 3000...');
-})
+app.all('*',  async () => {
+  throw new NotFoundError();
+});
+
+// - GLOBAL ERROR HANDLER -
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Auth service listening on port 3000...');
+  });
+};
+
+start();
---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+router.post('/api/users/signup', ((req: Request, res: Response) => {
+
+    res.send({});
+  }));
+
+export { router as signupRouter };
