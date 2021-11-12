798315165eade08dc5879dfb53142f9066fee630
Martin Metodiev <martin.metodiew@gmail.com>
2021-11-12 15:35:31 +0200

    feat(auth-srv): add sign in functionality

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,53 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../utils/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', ((req, res) => {
-  res.send('Hi there');
-}));
+router.post('/api/users/signin', [
+    body('email')
+      .isEmail()
+      .withMessage('Please provide a valid email'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('Password is required')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid email or password');
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store JWT on session object
+    req.session = {
+      jwt: userJwt
+    };
+
+    res.status(200).send(existingUser);
+  });
 
 export { router as signinRouter };
