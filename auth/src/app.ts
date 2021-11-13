ba08f4b19c985f3f3ca899c1e20f3dceb360f568
Martin Metodiev <martin.metodiew@gmail.com>
2021-11-13 12:54:26 +0200

    refactor(auth-srv): extract the app in a separate app.ts file

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'express';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+
+// Make sure express is aware we are behind nginx-ingress proxy
+app.set('trust proxy', true);
+
+// - GLOBAL MIDDLEWARE -
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+// - ROUTE HANDLERS -
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+// - GLOBAL ERROR HANDLER -
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,46 +1,9 @@
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-import { json } from 'express';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-
-// Make sure express is aware we are behind nginx-ingress proxy
-app.set('trust proxy', true);
-
-// - GLOBAL MIDDLEWARE -
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true
-  })
-);
-
-// - ROUTE HANDLERS -
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-// - GLOBAL ERROR HANDLER -
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY env var must be defined!');
+    throw new Error('JWT_KEY env var must be defined');
   }
 
   try {
