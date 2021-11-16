f6c1e00a6d059d04011c58cbb66259eec17b0525
Martin Metodiev <martin.metodiew@gmail.com>
2021-11-16 17:48:06 +0200

    refactor(client): add getInitialProps() call to the AppComponent

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-}
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  // Simple check for not all pages have defined getInitialProps()
+  if (appContext.Component.getInitialProps) {
+    // Because we're calling getInitialProps() on the AppComponent now we have to manually
+    // call the getInitialProps() on the Landing page and pass the context obj
+    // otherwise will not be invoked automatically
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    currentUser: data.currentUser
+  };
+};
+
+export default AppComponent;
